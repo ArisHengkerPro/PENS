c2763a3342ab36d51dd16c30946351c404070484
Aris Wahyu <56385559+Shaterpox@users.noreply.github.com>
2022-02-22 19:21:31 +0700

    Add files via upload
    Big O Notation, O(1)

---
+++ O(1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int jumlah(int a, int b){
+	return a+b;
+}
+
+int main(){
+	int a, b, hasil;
+	
+	cout << "Masukkan nilai a dan b : ";
+	cin >> a >> b;
+}
