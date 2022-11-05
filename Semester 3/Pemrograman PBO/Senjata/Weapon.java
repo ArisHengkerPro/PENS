9db36647cc961e3b32ebfda3c5b0ec1a556c2c54
Aris Wahyu <56385559+Ariswahyu02@users.noreply.github.com>
2022-11-05 20:59:50 +0700

    Add files via upload

---
+++ Semester 3/Pemrograman PBO/Senjata/Weapon.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Senjata;
+
+/**
+ *
+ * @author ASUS
+ */
+public abstract class Weapon {
+    
+    public abstract void Attack();
+    
+    public void Info(){
+        
+    }
+    
+}
